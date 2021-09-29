1aafc6564b0330db3531a66bbae64c8b9a71ad14
tedlopez <tedmlopez@gmail.com>
2021-09-28 21:44:59 -0400

    Video hero banner

--- lib/tedx_web/views/page_view.ex
+++ lib/tedx_web/views/page_view.ex
@@ -1,3 +1,9 @@
 defmodule TedxWeb.PageView do
   use TedxWeb, :view
+
+  def random_video do
+    videos = VideoLinks.videos()
+    random = Enum.random(0..Enum.count(videos))
+    Enum.at(videos, random)
+  end
 end
