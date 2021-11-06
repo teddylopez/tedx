847bda78b73adaef7b73f45c366822905d36f1e8
teddylopez <tedmlopez@gmail.com>
2021-11-05 21:23:48 -0400

    Add page titles

--- lib/tedx_web/controllers/portfolio_controller.ex
+++ lib/tedx_web/controllers/portfolio_controller.ex
@@ -2,12 +2,15 @@ defmodule TedxWeb.PortfolioController do
   use TedxWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html", projects: PortfolioProjects.all())
+    render(conn, "index.html",
+      projects: PortfolioProjects.all(),
+      page_title: "tedlopez | portfolio"
+    )
   end
 
   def show(conn, %{"id" => id}) do
     id = String.to_atom(id)
     project = PortfolioProjects.all()[id]
-    render(conn, "show.html", project: project)
+    render(conn, "show.html", project: project, page_title: "tedlopez | #{project.name}")
   end
 end
