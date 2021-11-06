aeea3c7b63aeaa6d4154bc8bb9ef5726c59033c0
teddylopez <tedmlopez@gmail.com>
2021-11-05 21:23:48 -0400

    Add page titles

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,11 +42,6 @@ import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let hooks = {};
-hooks.ConfirmClick = {
-  mounted() {
-    console.log("mounted!")
-  }
-}
 let liveSocket = new LiveSocket("/live", Socket, {
   params: {
     _csrf_token: csrfToken
--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :tedx, TedxWeb.Endpoint,
-  url: [scheme: "https", host: "tedxspace.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "tedlopez.us", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
