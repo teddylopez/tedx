93f9d5e3dfc5cedaf48a1cc908268e99ec0d3066
teddylopez <tedmlopez@gmail.com>
2021-11-08 14:58:45 -0500

    Fix websocket connection for production

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,12 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :tedx, TedxWeb.Endpoint,
-  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "tedlopez.us", port: 443],
+  check_origin: [
+    "https://tedlopez.us",
+    "https://www.tedlopez.us",
+    "https://tedxspace.herokuapp.com"
+  ],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
