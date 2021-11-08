b7eb63bedc5ac57757536481783c5d327c31fbc9
teddylopez <tedmlopez@gmail.com>
2021-11-07 19:53:26 -0500

    Troubleshoot production endpoint for websocket

--- config/prod.exs
+++ config/prod.exs
@@ -12,9 +12,8 @@ import Config
 config :tedx, TedxWeb.Endpoint,
   url: [scheme: "https", host: "tedlopez.us", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
-
-# secret_key_base: System.get_env("SECRET_KEY_BASE")
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
